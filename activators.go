4c8c8412ef85b417c0cdf6c0048cf2386886eafe
Karol Duleba <mr.fuxi@gmail.com>
2016-02-26 23:08:36 +0000

    Neuron activators

---
+++ activators.go
@@ -0,0 +1,46 @@
+package neural
+
+import "math"
+
+// Activator calculates neuron activation and it's derivative from given potential
+type Activator interface {
+	Activation(float64) float64
+	Derivative(float64) float64
+}
+
+// NewLinearActivator creates Activator that applies linear function to given potential
+// Actication: a*potential + 0
+// Derivative: a
+func NewLinearActivator(a float64) Activator {
+	return &linearActication{a}
+}
+
+type linearActication struct {
+	a float64
+}
+
+func (l *linearActication) Activation(potential float64) float64 {
+	return l.a * potential
+}
+
+func (l *linearActication) Derivative(potential float64) float64 {
+	return l.a
+}
+
+// NewSigmoidActivator creates Activator that applies linear function to given potential
+// Actication: a*potential + 0
+// Derivative: a
+func NewSigmoidActivator() Activator {
+	return &sigmoidActivator{}
+}
+
+type sigmoidActivator struct{}
+
+func (s *sigmoidActivator) Activation(potential float64) float64 {
+	return 1.0 / (1.0 + math.Exp(-potential))
+}
+
+func (s *sigmoidActivator) Derivative(potential float64) float64 {
+	activation := s.Activation(potential)
+	return activation * (1 - activation)
+}
