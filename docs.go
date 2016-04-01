46f0f8fa903663768637af7c969ecbebb1c7e180
Karol Duleba <karol@potatolondon.com>
2016-04-01 19:51:51 +0200

    Main docs

---
+++ docs.go
@@ -0,0 +1,5 @@
+// Package neural implements multi layer neural network of arbitrary number of layers.
+// Goal is to provide fast and flexible solution, to build both regular MLP and more soffisticated structures used for deep learning
+//
+// As much as possible training is done concurrently to utilize all CPU power
+package neural
