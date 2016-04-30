74323bd6bbb99b5719e41b5d0bcb5d072bc89984
Karol Duleba <mr.fuxi@gmail.com>
2016-04-30 23:27:33 +0100

    Docs for softmax activation function

--- activators.go
+++ activators.go
@@ -78,6 +78,10 @@ func (s *stepActicator) Derivative(dst, potentials []float64) {
 	}
 }
 
+// NewSoftmaxFunction creates Activator that scales responses in layer from 0 to 1
+// Sum of responses in layer are equal 1, so it can be interpret as probability
+// This activator should be used in last layer with Log Likelihood
+// Derivative is not implemented as it should not be needed. If used it will panic
 func NewSoftmaxFunction() Activator {
 	return &softmaxActicator{}
 }
